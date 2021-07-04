f4e22da9f722a5ed09c199fa24fc1b30c1b586c7
1wku <83960491+1wku@users.noreply.github.com>
2021-07-04 14:22:04 +0700

    style for tu du list

--- ToDo.js
+++ ToDo.js
@@ -1,16 +1,25 @@
 let addToDoButton = document.getElementById('addToDo');
 let toDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField');
+var blue = "#7EBB94";
 
 addToDoButton.addEventListener('click', function() {
-    var paragraph = document.createElement('p') ;
-    paragraph.innerText  = inputField.value;
-    toDoContainer.appendChild(paragraph);
-    inputField.value = "";
-    paragraph.addEventListener('click', function() {
-        paragraph.style.textDecoration = "line-through";
-    })
-    paragraph.addEventListener('dblclick', function() {
-        paragraph.style.textDecoration = "boder";
-    })
+    if (inputField.value != "") {
+        inputField.placeholder = "Thêm ghi chú mới ở đây!"
+        var paragraph = document.createElement('div') ;
+        toDoContainer.appendChild(paragraph);
+        paragraph.innerText  = "." + inputField.value;
+        paragraph.classList.add('newTask');
+        inputField.value = "";
+        paragraph.addEventListener('click', function() {
+            paragraph.classList.add('complete');
+
+        })
+
+    } else {
+        inputField.placeholder = "ơ kìa viết gì đó đi chứ!"
+        setTimeout(function() {
+            inputField.placeholder = "Nhanh lên nào!!";
+        },3000)
+    }
 })
